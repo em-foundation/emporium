d6ff61f8a1928914eb1b48102eb61bb3692a6d50
bob frankel <bob@biosbob.biz>
2026-01-20 19:39:21 -0800

    rp2040: ConsoleUart

--- workspace/bob.pkg/rpi.gist/Gist03.em.ts
+++ workspace/bob.pkg/rpi.gist/Gist03.em.ts
@@ -5,6 +5,7 @@ import * as $R from '@rpi.distro.2040/REGS.em'
 
 import * as BoardC from '@rpi.distro.2040/BoardC.em'
 import * as BusyWait from '@em.utils/BusyWait.em'
+import * as ConsoleUart from '@rpi.mcu.2040/ConsoleUart0.em'
 
 export const AppLedPin = $delegate(BoardC.AppLedPin)
 export const SysLedPin = $delegate(BoardC.SysLedPin)
@@ -20,8 +21,9 @@ export function em$run() {
     AppLedPin.clear()
     SysLedPin.makeOutput()
     SysLedPin.clear()
-    // GPIO
+    let b = c$`a`
     for (const _ of $range(10)) {
+        ConsoleUart.put(b++)
         AppLedPin.toggle()
         delayMs(200)        
     }
--- workspace/rpi.2040/rpi.distro.2040/BoardC.em.ts
+++ workspace/rpi.2040/rpi.distro.2040/BoardC.em.ts
@@ -2,9 +2,11 @@ import '@$$emscript'
 export const $U = $declare('COMPOSITE')
 
 import * as BusyWait from '@em.utils/BusyWait.em'
+import * as ConsoleUart from '@rpi.mcu.2040/ConsoleUart0.em'
 import * as GpioT from '@rpi.mcu.2040/GpioT.em'
 
 export const AppLedPin = $clone(GpioT)
+export const AppOutPin = $clone(GpioT)
 export const SysLedPin = $clone(GpioT)
 
 export const DEFAULTS = {
@@ -25,6 +27,8 @@ export function em$configure(): void {
     if ($isbare()) return
     const brd = $board(DEFAULTS)
     AppLedPin.pin_num.$$val = brd.pins.appLed
+    AppOutPin.pin_num.$$val = brd.pins.appOut
     BusyWait.scalar.$$val = 16
+    ConsoleUart.TxPin.$$dlg = AppOutPin
     SysLedPin.pin_num.$$val = brd.pins.sysLed
 }
