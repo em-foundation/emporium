4f10c09180c123f7897cd8a7c3964029919b915a
bob frankel <bob@biosbob.biz>
2026-01-20 16:57:10 -0800

    rp2040: genregs

--- workspace/rpi.2040/rpi.distro.2040/BoardC.em.ts
+++ workspace/rpi.2040/rpi.distro.2040/BoardC.em.ts
@@ -1,3 +1,16 @@
 import '@$$emscript'
 export const $U = $declare('COMPOSITE')
 
+export const DEFAULTS = {
+    activeLowLeds: false,
+    pins: {
+        appBut: <i16>-1,
+        appLed: <i16>-1,
+        appOut: <i16>-1,
+        sysDbgA: <i16>-1,
+        sysDbgB: <i16>-1,
+        sysDbgC: <i16>-1,
+        sysDbgD: <i16>-1,
+        sysLed: <i16>-1,
+    }
+}
