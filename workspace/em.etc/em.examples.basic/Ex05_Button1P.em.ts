841ed18cf37380b3ce760e65ef6bf88c58f3c0ed
bob frankel <bob@biosbob.biz>
2026-01-28 18:09:33 -0800

    Ex05_Button: clearDetect

--- workspace/em.etc/em.examples.basic/Ex05_Button1P.em.ts
+++ workspace/em.etc/em.examples.basic/Ex05_Button1P.em.ts
@@ -16,10 +16,10 @@ export namespace em$meta {
 export function em$startup() {
     AppButEdge.init(true)
     AppButEdge.setDetectFalling()
+    AppButEdge.clearDetect()
 }
 
 export function em$run() {
-    AppButEdge.clearDetect()
     Common.GlobalInterrupts.enable()
     while (true) {
         AppButEdge.enableDetect()
