15ff528fbfd442976625c3bf51ae4315e4c1b176
bob frankel <bob@biosbob.biz>
2026-03-04 10:42:27 -0800

    updated reset.sh

--- reset.sh
+++ reset.sh
@@ -16,5 +16,5 @@ echo 'Proceeding...'
 echo
 
 rm -rf $HOME/EM/data/* $HOME/EM/exts/* $HOME/EM/repo/*
-code --install-extension the-em-foundation.embrowser --install-extension Wokwi.wokwi-vscode --extensions-dir exts
+code --install-extension the-em-foundation.em-builder --install-extension Wokwi.wokwi-vscode --extensions-dir exts
 code --skip-welcome --user-data-dir "$HOME/EM/data" --extensions-dir "$HOME/EM/exts" 
