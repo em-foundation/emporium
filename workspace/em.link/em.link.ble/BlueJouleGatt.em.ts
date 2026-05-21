0fa1ed1b3425304bf91f8bd3bbc765f1eebfb2a5
bob frankel <bob@biosbob.biz>
2026-05-21 07:59:16 -0700

    initial schema

---
+++ workspace/em.link/em.link.ble/BlueJouleGatt.em.ts
@@ -0,0 +1,25 @@
+import '@$$emscript'
+export const $U = $declare('MODULE')
+
+export const $S_name = 'BlueJouleGattRw'
+export const $S_uuid = '0000b100-0000-1000-8000-00805f9b34fb'
+
+export type Status_T = u8
+export const Status_uuid = '0000b101-0000-1000-8000-00805f9b34fb'
+
+export type Command_T = u8
+export const Command_uuid = '0000b102-0000-1000-8000-00805f9b34fb'
+
+//>> ---- em$targ ---- <<//
+
+var status_val: Status_T = 0
+var command_val: Command_T = 0
+
+export function Status_read(): Status_T {
+    return status_val
+}
+
+export function Command_write(value: Command_T) {
+    command_val = value
+    status_val = value
+}
