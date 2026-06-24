3517c27cc6a31ca5443c2d24421c02f05ad65b2c
bob frankel <bob@biosbob.biz>
2026-06-23 18:59:50 -0700

    flpr: Mcu

--- workspace/nordic.nrf54/nordic.distro.nrf54.flpr/BoardC.em.ts
+++ workspace/nordic.nrf54/nordic.distro.nrf54.flpr/BoardC.em.ts
@@ -1,14 +1,22 @@
 import '@$$emscript'
 export const $U = $declare('COMPOSITE')
 
+import * as BoardController from '@em.utils/BoardController.em'
 import * as BusyWait from '@em.utils/BusyWait.em'
 import * as Common from '@em.mcu/Common.em'
+import * as Console from '@em.lang/Console.em'
 import * as ConsoleUart from '@nordic.mcu.nrf54.flpr/ConsoleUart3.em'
+import * as Debug from '@em.lang/Debug.em'
 import * as GpioT from '@nordic.mcu.nrf54/GpioT.em'
 import * as LedT from '@em.utils/LedT.em'
+import * as Mcu from '@nordic.mcu.nrf54.flpr/Mcu.em'
 
 export const AppLed = $clone(LedT)
 export const AppLedPin = $clone(GpioT)
+export const DbgA = $clone(GpioT)
+export const DbgB = $clone(GpioT)
+export const DbgC = $clone(GpioT)
+export const DbgD = $clone(GpioT)
 export const SysLed = $clone(LedT)
 export const SysLedPin = $clone(GpioT)
 
@@ -29,12 +37,25 @@ export const DEFAULTS = {
 export function em$configure(): void {
     if ($isbare()) return
     const brd = $board(DEFAULTS)
+    $using(BoardController)
+    $using(Console)
+    $using(Debug)
     AppLed.Pin.$$dlg = AppLedPin
     AppLed.active_low.$$val = brd.activeLowLeds
     AppLedPin.pin_num.$$val = brd.pins.appLed
+    BoardController.Led.$$dlg = SysLed
     BusyWait.scalar.$$val = 13
     Common.BusyWait.$$dlg = BusyWait
     Common.ConsoleUart.$$dlg = ConsoleUart
+    Common.Mcu.$$dlg = Mcu
+    DbgA.pin_num.$$val = brd.pins.sysDbgA
+    DbgB.pin_num.$$val = brd.pins.sysDbgB
+    DbgC.pin_num.$$val = brd.pins.sysDbgC
+    DbgD.pin_num.$$val = brd.pins.sysDbgD
+    Debug.DbgA.$$dlg = DbgA
+    Debug.DbgB.$$dlg = DbgB
+    Debug.DbgC.$$dlg = DbgC
+    Debug.DbgD.$$dlg = DbgD
     SysLed.Pin.$$dlg = SysLedPin
     SysLed.active_low.$$val = brd.activeLowLeds
     SysLedPin.pin_num.$$val = brd.pins.sysLed
