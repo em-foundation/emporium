6ff129c908ab6bc7321c210102d81418449d3e84
bob frankel <bob@biosbob.biz>
2026-06-23 20:40:40 -0700

    flpr: data initialization fix

--- workspace/nordic.nrf54/nordic.distro.nrf54/VprLaunchP.em.ts
+++ workspace/nordic.nrf54/nordic.distro.nrf54/VprLaunchP.em.ts
@@ -10,25 +10,33 @@ export namespace em$meta { }
 const DESC_ADDR = 0x0000_8000
 
 class Desc extends $struct {
-    loadAddr: ptr_t<u32>
-    loadSize: u32
+    codeAddr: ptr_t<u32>
+    codeSize: u32
+    dataAddr: ptr_t<u32>
+    dataSize: u32
     bssAddr: ptr_t<u32>
     bssSize: u32
 }
 
 export function em$run() {
+    const base = DESC_ADDR + $sizeof<Desc>()
     const desc = $cast2<$$<Desc>>(DESC_ADDR)
-    const dst = desc.$$.loadAddr
-    const src = $cast2<ptr_t<u32>>(DESC_ADDR + $sizeof<Desc>())
-    for (const i of $range(desc.$$.loadSize)) {
-        dst[i] = src[i]
+    const code_src = $cast2<ptr_t<u32>>(base)
+    const code_dst = desc.$$.codeAddr
+    for (const i of $range(desc.$$.codeSize)) {
+        code_dst[i] = code_src[i]
     }
-    const bss = desc.$$.bssAddr
+    const data_src = $cast2<ptr_t<u32>>(base + (desc.$$.codeSize * 4))
+    const data_dst = desc.$$.dataAddr
+    for (const i of $range(desc.$$.dataSize)) {
+        data_dst[i] = data_src[i]
+    }
+    const bss_dst = desc.$$.bssAddr
     for (const i of $range(desc.$$.bssSize)) {
-        bss[i] = 0
+        bss_dst[i] = 0
     }
     $reg32[0x50040530] |= 0x10
-    $R.VPR00.INITPC.$$ = $cast2<u32>(desc.$$.loadAddr)
+    $R.VPR00.INITPC.$$ = $cast2<u32>(desc.$$.codeAddr)
     $R.VPR00.CPURUN.$$ = 1
     e$`asm volatile ("wfi")`
 }
