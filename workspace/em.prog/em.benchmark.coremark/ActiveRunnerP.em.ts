45c63743fb25f932ddfc578fe20d9e284ef77237
bob frankel <bob@biosbob.biz>
2026-07-02 17:30:36 -0700

    tour-102-01: baseline

--- workspace/em.prog/em.benchmark.coremark/ActiveRunnerP.em.ts
+++ workspace/em.prog/em.benchmark.coremark/ActiveRunnerP.em.ts
@@ -7,7 +7,6 @@ import * as CoreBench from '@em.benchmark.coremark/CoreBench.em'
 import * as Utils from '@em.benchmark.coremark/Utils.em'
 
 const AppLed = $delegate(BoardC.AppLed)
-const CacheStats = $delegate(BoardC.CacheStats)
 
 const iterations = $config<u16>(10)
 
@@ -20,22 +19,13 @@ export function em$startup() {
 }
 
 export function em$run() {
-    AppLed.on()
-    Common.BusyWait.wait(1_000_000)
-    AppLed.off()
-    CacheStats.start()
+    AppLed.wink(1000)
     Common.UsCounter.start()
-    $['%%d+']
     for (const _ of $range(iterations)) {
         CoreBench.run(0)
     }
-    $['%%d-']
-    let usecs = Common.UsCounter.stop()
-    CacheStats.stop()
-    AppLed.on()
-    Common.BusyWait.wait(1_000_000)
-    AppLed.off()
-    CacheStats.print()
+    const usecs = Common.UsCounter.stop()
+    AppLed.wink(1000)
     printf`usecs = %d\n`(usecs)
     printf`list crc = %04x\n`(Utils.getCrc(Utils.Kind.LIST))
     printf`matrix crc = %04x\n`(Utils.getCrc(Utils.Kind.MATRIX))
