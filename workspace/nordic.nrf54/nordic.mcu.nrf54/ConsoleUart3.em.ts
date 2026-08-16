59f5b177e9280f707376ebc53c4ec7d5a82ea26e
bob frankel <bob@biosbob.biz>
2026-08-16 15:58:49 -0700

    nordic, ti distros

---
+++ workspace/nordic.nrf54/nordic.mcu.nrf54/ConsoleUart3.em.ts
@@ -0,0 +1,53 @@
+import '@$$emscript'
+export const $U = $declare('MODULE', ConsoleUartI)
+
+import * as $R from '@nordic.distro.nrf54/REGS.em'
+
+import * as Common from '@em.mcu/Common.em'
+import * as ConsoleUartI from '@em.hal/ConsoleUartI.em'
+import * as GpioI from '@em.hal/GpioI.em'
+
+export const TxPin = $proxy<GpioI.$I>()
+
+export namespace em$meta {
+    export function em$construct() {
+        Common.Idle.em$meta.addSleepEnter($cb(sleepEnter))
+        Common.Idle.em$meta.addSleepLeave($cb(sleepLeave))
+    }
+}
+
+//>> ---- em$targ ---- <<//
+
+var txd: u8
+
+export function em$startup() {
+    sleepLeave()
+}
+
+export function flush() {
+    while ($R.UARTE30.EVENTS_DMA.TX.END.$$ == 0) { }
+    $R.UARTE30.EVENTS_DMA.TX.END.$$ = 1
+}
+
+export function put(data: u8) {
+    txd = data
+    $R.UARTE30.EVENTS_DMA.TX.END.$$ = 0
+    $R.UARTE30.TASKS_DMA.TX.START.$$ = 1
+    flush()
+}
+
+function sleepEnter() {
+    $R.UARTE30.ENABLE.$$ = $R.UARTE_ENABLE_ENABLE_Disabled
+    $R.UARTE30.PSEL.TXD.$$ = ~0
+    TxPin.reset()
+}
+
+function sleepLeave() {
+    $R.UARTE30.PSEL.TXD.$$ = 0  /// TODO: fix hard-coded P0.00
+    $R.UARTE30.BAUDRATE.$$ = $R.UARTE_BAUDRATE_BAUDRATE_Baud115200
+    $R.UARTE30.ENABLE.$$ = $R.UARTE_ENABLE_ENABLE_Enabled
+    //
+    e$`NRF_UARTE30_S->DMA.TX.PTR = (uint32_t)&txd`
+    $R.UARTE30.DMA.TX.MAXCNT.$$ = 1
+    $R.UARTE30.EVENTS_DMA.TX.END.$$ = 1
+}
