28227b35b1b33d25b19550de74b756722860f6bd
bob frankel <bob@biosbob.biz>
2026-08-24 07:59:18 -0700

    nrf91: sram

--- workspace/nordic.nrf91/nordic.distro.nrf91/BuildC.em.ts
+++ workspace/nordic.nrf91/nordic.distro.nrf91/BuildC.em.ts
@@ -94,9 +94,9 @@ export function em$generate() {
     LinkerC.genScript({
         dmem_flash: { orig: 0x20000000, len: 0x00004000 },
         imem_flash: { orig: 0x00000000, len: 0x00008000 },
-        dmem_sram: LinkerC.MEM_NULL,
-        imem_sram: LinkerC.MEM_NULL,
-        lmem_sram: LinkerC.MEM_NULL,
+        dmem_sram: { orig: 0x20000000, len: 0x00004000 },
+        imem_sram: { orig: 0x20004000, len: 0x00008000 },
+        lmem_sram: { orig: 0x00000000, len: 0x00008000 },
         cmem_sram: LinkerC.MEM_NULL,
     })
     let opt = $property('em.build.Optimize', 'Oz')
