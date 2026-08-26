28086d5aaafbb6352c535b5b416153ac8642dc3c
bob frankel <bob@biosbob.biz>
2026-08-26 06:32:27 -0700

    nordic.modem.nrf91

---
+++ workspace/nordic.nrf91/nordic.modem.nrf91/Modem.em.ts
@@ -0,0 +1,74 @@
+import '@$$emscript'
+export const $U = $declare('MODULE')
+
+import * as $R from '@nordic.distro.nrf91/REGS.em'
+
+import * as Common from '@em.mcu/Common.em'
+import * as IntrVec from '@em.arch.arm/IntrVec.em'
+
+import * as T from '@nordic.modem.nrf91/Types.em'
+
+var shmem_tab = $table<T.SharedMem>()
+
+export namespace em$meta {
+    export function em$construct() {
+        IntrVec.em$meta.useIntr('IPC')
+        const shmem = shmem_tab.$$add()
+        const ctrl = shmem.$$.ctrl
+        ctrl.host.version = T.CTRL_VERSION
+        ctrl.list_a.count = T.NUM_DESCS
+        ctrl.list_b.count = T.NUM_DESCS
+        for (const i of $range(T.NUM_DESCS)) {
+            ctrl.list_a.descs[i].state = T.DESC_FREE
+            ctrl.list_b.descs[i].state = T.DESC_FREE
+        }
+        ctrl.host.data_size = T.SHMEM_SIZE
+    }
+}
+
+//>> ---- em$targ ---- <<//
+
+export function em$startup() {
+    shmemConstruct()
+}
+
+export function handshake() {
+    modemStart()
+    waitForHandshake()
+}
+
+export function init() {
+    platformInit()
+    transportInit()
+}
+
+function ipcInit() {
+}
+
+function modemStart() {
+}
+
+function platformInit() {
+}
+
+function shmemConstruct() {
+    const shmem = $$(shmem_tab[0])
+    const ctrl = $$(shmem.$$.ctrl)
+    const rx = $$(shmem.$$.rx)
+    ctrl.$$.host.data_base = $cast2<u32>(rx)
+    ctrl.$$.host.list_a = $$(ctrl.$$.list_a)
+    ctrl.$$.host.list_b = $$(ctrl.$$.list_b)
+    ctrl.$$.host.modem_hdr = $cast2<u32>($$(ctrl.$$.modem))
+}
+
+function transportInit() {
+    shmemConstruct()
+    txHeapInit()
+    ipcInit()
+}
+
+function txHeapInit() {
+}
+
+function waitForHandshake() {
+}
---
+++ workspace/nordic.nrf91/nordic.modem.nrf91/Types.em.ts
@@ -0,0 +1,87 @@
+import '@$$emscript'
+export const $U = $declare('MODULE')
+
+export const NUM_DESCS = 8
+export const MSG_SIZE = 0x40
+export const MSG_WORDS = MSG_SIZE / 4
+export const CTRL_SIZE = 0x4E8
+
+export const CTRL_VERSION = 0x00010000
+export const DESC_FREE = 3
+
+export class Msg extends $vector<u32> {
+    $len = MSG_WORDS
+}
+
+export class MsgVec extends $vector<Msg> {
+    $len = NUM_DESCS
+}
+
+export class Desc extends $struct {
+    state: u32
+    msg: $$<Msg>
+}
+
+export class DescVec extends $vector<Desc> {
+    $len = NUM_DESCS
+}
+
+export class RpcList extends $struct {
+    count: u32
+    descs: DescVec
+}
+
+export class HostHdr extends $struct {
+    version: u32
+    data_base: u32
+    data_size: u32
+    list_a: $$<RpcList>
+    list_b: $$<RpcList>
+    modem_hdr: u32
+    reserved0: u32
+    reserved1: u32
+}
+
+export class ModemHdrRest extends $vector<u32> {
+    $len = 13
+}
+
+export class ModemHdr extends $struct {
+    state: u32
+    ptr0: u32
+    ptr1: u32
+    rest: ModemHdrRest
+}
+
+export class Ctrl extends $struct {
+    host: HostHdr          // +0x000, size 0x20
+    modem: ModemHdr        // +0x020, size 0x40
+    list_a: RpcList        // +0x060, size 0x44
+    list_b: RpcList        // +0x0A4, size 0x44
+    msgs_a: MsgVec         // +0x0E8, size 0x200
+    msgs_b: MsgVec         // +0x2E8, size 0x200
+}
+
+export const SHMEM_SIZE = 0x2000
+export const SHMEM_WORDS = SHMEM_SIZE / 4
+export const TX_RX_PAD_SIZE = 0x80
+export const TX_RX_PAD_WORDS = TX_RX_PAD_SIZE / 4
+
+export class ShmemArea extends $vector<u32> {
+    $len = SHMEM_WORDS
+}
+
+export class TxRxPad extends $vector<u32> {
+    $len = TX_RX_PAD_WORDS
+}
+
+export class SharedMem extends $struct {
+    ctrl: Ctrl             // +0x0000, size 0x04E8
+    tx: ShmemArea          // +0x04E8, size 0x2000
+    pad: TxRxPad           // +0x24E8, size 0x0080
+    rx: ShmemArea          // +0x2568, size 0x2000
+}
+
+export namespace em$meta { }
+
+//>> ---- em$targ ---- <<//
